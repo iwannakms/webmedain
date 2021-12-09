dcbbeaf2d175254abcbae699a6476dbbd891544b
nuradil <696.overlord.696@gmail.com>
2021-12-09 22:37:21 +0600

    Initial commit

---
+++ src/main/java/com/example/webmedain/WebmedainApplication.java
@@ -0,0 +1,13 @@
+package com.example.webmedain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmedainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebmedainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webmedain/WebmedainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webmedain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebmedainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
